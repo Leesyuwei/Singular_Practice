113baa7722dd0cc515fc5bf90f4d759d5d891bc4
Unknown <unknown@example.com>
2020-10-25 11:40:38 +0800

    <feat> has bug

---
+++ python/servo1025.py
@@ -0,0 +1,20 @@
+from microbit import *
+
+class servo:
+    def __init__(self,pin):
+        self.degree=0
+        self.pin=pin
+        self.pin.set_analog_period(20)
+        self.angle=0
+
+    def angle(self,idegree=None):
+        if idegree==None:
+            self.degree=0
+        else:
+            self.degree=idegree
+
+        self.pin.write_analog(int((0.5+(self.degree/90))*(1023/20)))
+
+servo1=servo(pin0)
+
+servo1.
